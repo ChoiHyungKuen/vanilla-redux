54de5d2192cb1d8c377aef14f8592a8ee0d73b51
gudrms1592 <gudrms1592@naver.com>
2020-12-23 22:48:18 +0900

    d

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+number.innerText = 0;
+
+// 이렇게 변수로 쓰면 오타를 쳤을때 에러가 발생함..
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+const countModifier = (count = 0, action) => {
+  console.log(count, action)
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count -1;
+    default:
+      return count;
+  }
+}
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  console.log('스토어가 변경됬습니다~ ' + countStore.getState())
+  number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange)
+
+const handleAdd = () => {
+  countStore.dispatch({type: ADD})
+}
+
+const handleMinus = () => {
+  countStore.dispatch({type: MINUS})
+}
+
+add.addEventListener('click', handleAdd );
+minus.addEventListener('click', handleMinus);
+
